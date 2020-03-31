09bce758a3b2832286d43e441dba6a58fe4b45fc
anuj-prgrmng <anujh105@gmail.com>
2020-03-31 13:58:01 +0530

    Commit for led_blink project and doc related to it.

---
+++ Project/Project_Led_Blink/Objects/led.d
@@ -0,0 +1,15 @@
+.\objects\led.o: led.c
+.\objects\led.o: C:\Users\ANUJ\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\stm32f10x.h
+.\objects\led.o: .\RTE\_Target_1\RTE_Components.h
+.\objects\led.o: C:\Users\ANUJ\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm3.h
+.\objects\led.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\led.o: C:\Users\ANUJ\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\led.o: C:\Users\ANUJ\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\led.o: C:\Users\ANUJ\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\led.o: C:\Users\ANUJ\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\system_stm32f10x.h
+.\objects\led.o: .\RTE\Device\STM32F103C8\stm32f10x_conf.h
+.\objects\led.o: C:\Users\ANUJ\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_flash.h
+.\objects\led.o: C:\Users\ANUJ\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\stm32f10x.h
+.\objects\led.o: C:\Users\ANUJ\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_gpio.h
+.\objects\led.o: C:\Users\ANUJ\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_rcc.h
+.\objects\led.o: C:\Users\ANUJ\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\misc.h
---
+++ Project/Project_Led_Blink/led.c
@@ -0,0 +1,56 @@
+#include "stm32f10x.h"
+GPIO_InitTypeDef GPIO_InitStructure; /*Declare Structure Variable and right-click to goto structure definition*/
+static __IO uint32_t TimingDelay; /*Declare a variable for delay*/
+
+/*GPIO Init function Declaration and Definition*/  
+void InitGPIO()
+{
+	/*Function to Enable or Disable the APB2 and right-click to goto function definition*/
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	/*Initialize Structure variables*/
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	/*Function call for GPIO_Init*/
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+}
+
+/*Timer Init function Declartion and Definition*/
+void InitTimer()
+{
+	/*Function call for Configuring Sys Tick and SystemCoreClock = 72MHz*/
+	SysTick_Config(SystemCoreClock / 1000);
+}
+
+/*Free Running function for Handling SysTick*/
+void SysTick_Handler(void)
+{
+	if (TimingDelay != 0x00)
+		{
+			TimingDelay--;
+		}
+}
+
+/*Delay function Declartion and Definition */
+void Delay(__IO uint32_t nTime)
+{
+	TimingDelay = nTime;
+	while(TimingDelay != 0);
+}
+
+/*Main function*/
+int main()
+{
+	InitGPIO();
+	InitTimer();
+	
+	while(1)
+		{
+			/*SET Pin C13*/
+			GPIOC->BSRR = GPIO_Pin_13;
+			Delay(1000);
+			/*CLEAR Pin C13*/
+			GPIOC->BRR  = GPIO_Pin_13;
+			Delay(1000);
+		}
+}
